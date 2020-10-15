238a610ebeb2a8440379c3c4c4c0c3fa82a91e61
Daivalentineno Janitra Salim <35824236+daivalentinenojs@users.noreply.github.com>
2020-10-15 22:40:26 +0800

    Add files via upload

--- Section 2 – Basics And Core Concepts – DOM Interaction/Chapter 26 – Chapter 31/Basics-05 Using The Native Event Object (Own)/app.js
+++ Section 2 – Basics And Core Concepts – DOM Interaction/Chapter 26 – Chapter 31/Basics-05 Using The Native Event Object (Own)/app.js
@@ -1,1 +1,61 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 0,
+      name: '',
+      lastName: '',
+      // fullname: ''
+    };
+  },
+  watch: {
+    counter(value) {
+      if (value > 50) {
+        const that = this;
+        setTimeout(function () {
+          that.counter = 0;
+        }, 2000);        
+      }
+    }
+  //   name(value) {
+  //     if (value === '') {
+  //       this.fullname = '';
+  //     } else {
+  //       this.fullname = value + ' ' + this.lastName;
+  //     }
+  //   },
+  //   lastName(value) {
+  //     if (value === '') {
+  //       this.lastName = '';
+  //     } else {
+  //       this.fullname = this.name + ' ' + value;
+  //     }
+  //   }
+  },
+  computed: {
+    fullname() {
+      if (this.name === '' || this.lastName === ''){
+        return '';
+      }
+      return this.name + ' ' + this.lastName;
+    },
+  },
+  methods: {
+    resetInput() {
+      this.name = '';
+      this.lastName = '';
+    },
+    setName(event, lastName) {
+      this.name = event.target.value;
 
+    },
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    reduce(num) {
+      this.counter = this.counter - num;
+      // this.counter--;
+    }
+  }
+});
+
+app.mount('#events');
