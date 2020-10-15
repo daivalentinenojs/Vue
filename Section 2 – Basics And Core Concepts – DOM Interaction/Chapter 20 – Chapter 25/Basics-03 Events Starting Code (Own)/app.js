3e20fed30d5a6a3080bdaa9faa8a6c1e0429839c
Daivalentineno Janitra Salim <35824236+daivalentinenojs@users.noreply.github.com>
2020-10-15 22:39:02 +0800

    Add files via upload

--- Section 2 – Basics And Core Concepts – DOM Interaction/Chapter 20 – Chapter 25/Basics-03 Events Starting Code (Own)/app.js
+++ Section 2 – Basics And Core Concepts – DOM Interaction/Chapter 20 – Chapter 25/Basics-03 Events Starting Code (Own)/app.js
@@ -1,1 +1,32 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      counter: 10,
+      name: '',
+      confirmedName: ''
+    };
+  },
+  methods: {
+    confirmInput() {
+      this.confirmedName = this.name;
+    },
+    submitForm () {
+      alert('Submitted !')
+    },
+    // submitForm (event) {
+    //   event.preventDefault();
+    //   alert('Submitted !')
+    // },
+    setName (event, lastName) {
+      this.name = event.target.value + ' ' + lastName;
+    },
+    add (num) {
+      this.counter += num;
+    },
+    reduce (num) {
+      this.counter -= num;
+    }
+  }
+});
 
+app.mount('#events');
