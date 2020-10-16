2900be4448144b3f6e14abe8e8362e5283851fee
Daivalentineno Janitra Salim <35824236+daivalentinenojs@users.noreply.github.com>
2020-10-16 19:00:25 +0800

    Section 3 - Chapter 39 – Chapter 46
    Section 3 - Chapter 39 – Chapter 46

---
+++ Section 3 – Rendering Conditional Content and Lists/Chapter 39 – Chapter 46/Lists-Cond-01 Starting Set Up (Own)/app.js
@@ -0,0 +1,19 @@
+const app = Vue.createApp({
+  data() {
+    return { 
+      enteredGoalValue: '',
+      goals: []
+    };
+  },
+  methods: {
+    addGoal() {
+      this.goals.push(this.enteredGoalValue);
+      this.enteredGoalValue = '';
+    },
+    removeGoal (idx) {
+      this.goals.splice(idx, 1);
+    }
+  }
+});
+
+app.mount('#user-goals');
