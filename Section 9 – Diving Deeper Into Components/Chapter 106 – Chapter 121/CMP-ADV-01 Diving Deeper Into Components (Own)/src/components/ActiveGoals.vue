575b2930e7e1fa269aa33342b64d80c8446efe49
Daivalentineno Janitra Salim <35824236+daivalentinenojs@users.noreply.github.com>
2020-10-21 23:32:17 +0800

    Section 9 - Chapter 106 – Chapter 121
    Section 9 - Chapter 106 – Chapter 121

---
+++ Section 9 – Diving Deeper Into Components/Chapter 106 – Chapter 121/CMP-ADV-01 Diving Deeper Into Components (Own)/src/components/ActiveGoals.vue
@@ -0,0 +1,21 @@
+<template>
+    <h2>Active Goals</h2>
+</template>
+
+<badge-list></badge-list>
+<user-info
+    :full-name="activeUser.name"
+    :info-text="activeUser.description"
+    :role="activeUser.role"
+></user-info>
+<course-goals>
+    <template #default="slotProps">
+    <h2>{{ slotProps.item }}</h2>
+    <p>{{ slotProps['another-prop'] }}</p>
+    </template>
+</course-goals>
+
+<!-- <course-goals #default="slotProps">
+    <h2>{{ slotProps.item }}</h2>
+    <p>{{ slotProps['another-prop'] }}</p>
+</course-goals> -->
---
+++ Section 9 – Diving Deeper Into Components/Chapter 106 – Chapter 121/CMP-ADV-01 Diving Deeper Into Components (Own)/src/components/UserInfo.vue
@@ -0,0 +1,20 @@
+<template>
+  <section>
+    <base-card>
+      <!-- <template v-slot:header> -->
+      <template #header>
+        <h3>{{ fullName }}</h3>
+        <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
+      </template>
+      <template #default>
+        <p>{{ infoText }}</p>
+      </template>
+    </base-card>    
+  </section>
+</template>
+
+<script>
+export default {
+  props: ['fullName', 'infoText', 'role'],
+};
+</script>
