8e3b7a3c653b48c6bd0968bf53a2c574eab797ce
Daivalentineno Janitra Salim <35824236+daivalentinenojs@users.noreply.github.com>
2020-10-30 00:14:55 +0800

    Section 22 - Chapter 317 – Chapter 323
    Section 22 - Chapter 317 – Chapter 323

---
+++ Section 22 – Vue 2 To Vue 3 Migration/Chapter 317 – Chapter 323/Vue-3 Demo Project 1 (Own)/router-v3/src/main.js
@@ -0,0 +1,23 @@
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+
+import App from './App.vue';
+import WelcomeScreen from './pages/WelcomeScreen.vue';
+import UsersList from './pages/UsersList.vue';
+
+// Add router!
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        { path: '/', component: WelcomeScreen },
+        { path: '/users', component: UsersList },
+    ],
+});
+
+const app = createApp(App);
+app.use(router);
+
+router.isReady().then(() => {
+    app.mount('#app');
+});
+
