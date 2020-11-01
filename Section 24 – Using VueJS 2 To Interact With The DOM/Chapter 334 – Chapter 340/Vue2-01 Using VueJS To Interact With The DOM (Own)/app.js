2d876aaed04d405f16afaaf52701cc4da92f8420
Daivalentineno Janitra Salim <35824236+daivalentinenojs@users.noreply.github.com>
2020-11-01 22:06:02 +0800

    Section 24 - Chapter 334 – Chapter 340
    Section 24 - Chapter 334 – Chapter 340

---
+++ Section 24 – Using VueJS 2 To Interact With The DOM/Chapter 334 – Chapter 340/Vue2-01 Using VueJS To Interact With The DOM (Own)/app.js
@@ -0,0 +1,13 @@
+// Put this in the script section in JSFiddle
+// In a local setup, you need to merge this and the index.html file into one file
+new Vue({
+	el: '#app',
+	data: {
+		title: 'Hello World!'
+	},
+	methods: {
+		sayHello: function() {
+			return this.title;
+		}
+	}
+});
