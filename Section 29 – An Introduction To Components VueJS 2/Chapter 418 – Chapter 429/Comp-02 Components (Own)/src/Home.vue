1d1e776f137c6e687f846d32b4682c85bd3414cf
Daivalentineno Janitra Salim <35824236+daivalentinenojs@users.noreply.github.com>
2020-11-04 09:21:23 +0800

    Section 29 - Chapter 418 – Chapter 429
    Section 29 - Chapter 418 – Chapter 429

---
+++ Section 29 – An Introduction To Components VueJS 2/Chapter 418 – Chapter 429/Comp-02 Components (Own)/src/Home.vue
@@ -0,0 +1,15 @@
+<template>   
+    <div>
+        <app-server-status v-for="server in 5"></app-server-status>
+    </div>
+</template>
+
+<script>
+import ServerStatus from './ServerStatus.vue';
+
+export default {
+    component: {
+        'app-server-status': ServerStatus
+    }
+}
+</script>
