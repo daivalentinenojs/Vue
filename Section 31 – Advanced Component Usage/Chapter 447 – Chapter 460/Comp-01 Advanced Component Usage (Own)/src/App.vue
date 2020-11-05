7f685a51b962f313977fd60b3d882cc983e24331
Daivalentineno Janitra Salim <35824236+daivalentinenojs@users.noreply.github.com>
2020-11-05 11:53:44 +0800

    Section 31 - Chapter 447 – Chapter 460
    Section 31 - Chapter 447 – Chapter 460

---
+++ Section 31 – Advanced Component Usage/Chapter 447 – Chapter 460/Comp-01 Advanced Component Usage (Own)/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12">
+                <button @click="selectedComponent = 'appQuote'">Quote</button>
+                <button @click="selectedComponent = 'appAuthor'">Author</button>
+                <button @click="selectedComponent = 'appNew'">New Quote</button>
+                <hr>
+                <p>{{ selectedComponent }}</p>
+                <keep-alive>
+                    <component :is="selectedComponent">
+                    <!-- This code below will be the defult content,
+                    if the component children dont have content -->
+                    <p>Default Content</p>
+                    </component>
+                </keep-alive>
+                <!-- <app-quote>
+                    <h2 slot="title">{{ quoteTitle }}</h2>
+                    <p>A Wonderful Quote</p>
+                </app-quote> -->
+                <hr>
+                <app-author></app-author>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+import Queue from './components/Quote.vue';
+import Author from './components/Quote.vue';
+import NewQuote from './components/Quote.vue';
+
+export default {
+    data: function() {
+        return {
+            quoteTitle: 'The Quote',
+            selectedComponent: 'appQuote'
+        }
+    },
+    components: {
+        appQuote: Quote,
+        appAuthor: Author,
+        appNew: NewQuote
+    }
+}
+</script>
+
+<style scoped>
+h2 {
+    color: red;
+}
+</style>
